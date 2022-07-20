a0daa592fff13f46416877225d8d26d660365f67
salsabilaathika-97 <salsathikar@gmail.com>
2022-07-20 21:12:21 +0800

    practice add inline styling in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,21 @@
 const Navbar = () => {
     return (
-      <>
+      <div style = {{ 
+        display: "flex",
+        justifyContent: "space-around"
+        }}>
         <div>
             <h1>Logo</h1>
         </div>
-        <div>
+        <div style = {{ 
+            display: "flex"
+         }}>
             <p>Our Services</p>
             <p>Why Us</p>
             <p>Testimonial</p>
             <p>FAQ</p>
         </div>
-      </>
+      </div>
     )
   }
   
