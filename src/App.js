b66658114560685b52c22a9c331114afc1cb1fd3
salsabilaathika-97 <salsathikar@gmail.com>
2022-07-20 22:36:29 +0800

    simplify calling props

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import Navbar from "./components/Navbar";
 const  navList = ['our service', 'why us', 'testimonial', 'faq']
 const image = 'Logo';
 
+const props = {
+  navList,
+  image,
+}
+
 const App = () => {
   return (
     <div>
-      <Navbar propNav = {navList} propImage = {image} />
+      <Navbar {...props} />
     </div>
   )
 }
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,18 +1,18 @@
 import "./style.css"
 
 const Navbar = (props) => {
-    const {propNav, propImage} = props;
+    const {navList, image} = props;
     return (
       <div className = "navbar-body">
       <div className = "navbar-row">
         <div>
-          <h1>{propImage}</h1>
+          <h1>{image}</h1>
         </div>
         <div className = "nav-list">
-            <p>{propNav[0]}</p>
-            <p>{propNav[1]}</p>
-            <p>{propNav[2]}</p>
-            <p>{propNav[3]}</p>
+            <p>{navList[0]}</p>
+            <p>{navList[1]}</p>
+            <p>{navList[2]}</p>
+            <p>{navList[3]}</p>
         </div>
       </div>
       </div>
