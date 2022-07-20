12be540b4c72f46066c4a11d2e2cba2643a30247
salsabilaathika-97 <salsathikar@gmail.com>
2022-07-20 21:08:11 +0800

    add navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+const Navbar = () => {
+    return (
+      <>
+        <div>
+            <h1>Logo</h1>
+        </div>
+        <div>
+            <p>Our Services</p>
+            <p>Why Us</p>
+            <p>Testimonial</p>
+            <p>FAQ</p>
+        </div>
+      </>
+    )
+  }
+  
+  export default Navbar;
+  
