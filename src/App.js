c3aec82c6ae83d138e086e0db452f8e1840b3f01
salsabilaathika-97 <salsathikar@gmail.com>
2022-07-21 21:10:31 +0800

    add state for handle sidebar icon

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Navbar from "./components/Navbar";
 import Strength from "./components/Strength";
 import { navList, image, strengthList } from "./const/staticData";
-
+import {useState} from "react";
 
 const props = {
   navList,
@@ -10,10 +10,21 @@ const props = {
 }
 
 const App = () => {
+
+  const [showSidebar, setshowSidebar] = useState(false);
+
+  console.log("ini sidebar",showSidebar);
+
+  const handleSidebar = () => {
+    console.log("fungsi berjalan");
+    setshowSidebar(!showSidebar);
+  }
+
   return (
     <div>
       {/* <Navbar {...props} /> */}
       <Strength {...props} />
+      <h1 onClick={handleSidebar}>{showSidebar === true ? "x" :"="}</h1>
     </div>
   )
 }
